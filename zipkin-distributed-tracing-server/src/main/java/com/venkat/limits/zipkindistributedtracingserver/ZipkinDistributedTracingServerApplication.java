5ee8165639e85cef6dd776cc728c562620f37cc4
venkatram64 <venkat.veerareddy@hotmail.com>
2019-04-26 06:38:58 +0530

    init commit

--- zipkin-distributed-tracing-server/src/main/java/com/venkat/limits/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
+++ zipkin-distributed-tracing-server/src/main/java/com/venkat/limits/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
@@ -2,8 +2,10 @@ package com.venkat.limits.zipkindistributedtracingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
 public class ZipkinDistributedTracingServerApplication {
 
 	public static void main(String[] args) {
